8302c38a60a7d5a453c18e7d4c54ec7ac899005e
devgsanches <guilhermesanches.pessoal@gmail.com>
2025-09-10 11:26:32 -0300

    fix: add url database fallback and jwt secret for tests

--- src/env/index.ts
+++ src/env/index.ts
@@ -4,8 +4,11 @@ import { z } from 'zod'
 const envSchema = z.object({
   NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev'),
   PORT: z.coerce.number().default(3333),
-  DATABASE_URL: z.string().url(),
-  JWT_SECRET: z.string(),
+  DATABASE_URL: z
+    .string()
+    .url()
+    .default('postgresql://user:password@localhost:5432/default_db'), // fallback database url for testing
+  JWT_SECRET: z.string().default('secret'),
 })
 
 export const _env = envSchema.safeParse(process.env)
